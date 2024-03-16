1d3cfaf6bbe91d78609e0a7a18d84d4b8c9768ba
Piyush Bansal <piyush043@gmail.com>
2024-03-16 11:03:42 -0700

    Initial commit of frontend app
    Added basic angular application

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Welcome to {{title}}!</h1>    
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'Stock App';
+}
