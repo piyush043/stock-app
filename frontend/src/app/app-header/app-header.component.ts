437e5f6dc26e901effe68a128a4c93be2660b15e
Piyush Bansal <piyush043@gmail.com>
2024-03-18 06:50:50 -0700

    Add logic to reset backend data

--- frontend/src/app/app-header/app-header.component.ts
+++ frontend/src/app/app-header/app-header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StockService } from '../stock.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-header.component.css']
 })
 export class AppHeaderComponent {
+  
   title: string = "Stock App";
+  showModal: boolean = false;
+  
+  constructor(private stockService:StockService) {}
+
+  resetAppData() {
+    this.stockService.resetData().subscribe({
+      next: (res) => {
+        // Setting value for reset data subject, so that it can multi-cast to its subscribers. 
+        this.stockService.setSubjectResetData(true);
+      }, 
+      error: (error) =>console.error(error),
+      complete: () => this.hideShowModal()
+    });
+  }
+
+  hideShowModal() {
+    this.showModal = !this.showModal;
+  }
 }
