ea09718a678230d4db1ac2262a5624cae89e147e
Piyush Bansal <piyush043@gmail.com>
2024-03-18 06:15:39 -0700

    Update stock-list component
    - Add pagination
    - Add alert
    - Use stock service to get all stock & delete

--- frontend/src/app/stock-list/stock-list.component.ts
+++ frontend/src/app/stock-list/stock-list.component.ts
@@ -1,14 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StockService } from '../stock.service';
+import { ClrDatagridSortOrder } from '@clr/angular';
+import { Stock } from '../interfaces';
 
 @Component({
   selector: 'stock-list',
   templateUrl: './stock-list.component.html',
   styleUrls: ['./stock-list.component.css']
 })
-export class StockListComponent {
+export class StockListComponent implements OnInit{
+  
+  stocks:Stock[] = [];
+  ascSort = ClrDatagridSortOrder.ASC;
+  alertText:string = "";
+  alertType:string = "success";
+  currFilter: string = '';
+
   constructor(private stockService: StockService) {}
 
-  stocks = this.stockService.getAllStocks();
+  ngOnInit() {
+    this.getAllStocks();
+    
+  }
+
+  getAllStocks (): void {
+    this.stockService.getStocks().subscribe({
+      next: (res) => {
+        this.stocks = res;
+        this.stocks.map((item) => item.selected=false);
+      },
+      error: (err) => console.error(err),
+      complete: () => {}
+    });
+  }
+
+  deleteStock(stock:Stock): void {
+    const stockId: number = stock.id;
+    this.stockService.deleteStock(stockId).subscribe({
+      next: (res) => {
+        this.alertText = res["msg"];
+        this.alertType = "success";
+      }, 
+      error: (error) => {
+        this.alertText = error.error["msg"];
+        this.alertType = "danger";
+        console.error(error);
+      },
+      complete: () => {
+        setTimeout(() => {
+          this.onCloseAlert();
+        }, 4000);
+      }
+    });
+  }
+
+  onCloseAlert(): void {
+    this.alertText = "";
+  }
+
+  onMouseOverDelete (stock: Stock): void {
+    stock.selected = true;
+  }
+  
+  onMouseLeaveDelete (stock: Stock): void {
+    stock.selected = false;
+  }
+
+  onShowStockDetails(stock: Stock): void {
+  }
+
 
 }
