a8e28c0ae043f90828546c4890baf8abe257041f
Piyush Bansal <piyush043@gmail.com>
2024-03-18 00:22:25 -0700

    [backend] add helper functions

--- server/stock.js
+++ server/stock.js
@@ -4,5 +4,35 @@ const fs = require('fs');
 
 const filePath = './data/stocks.json';
 
+/* 
+Helper function to read json file located at `filePath` 
+and return `callback` with jsonData or error.
+*/
+function jsonReader(filePath, callback) {
+  fs.readFile(filePath, 'utf8', (err, jsonData) => {
+    if (err) {
+      return callback && callback(err);
+    }
+    try {
+      const obj = JSON.parse(jsonData);
+      return callback && callback(null, obj);
+    } catch(err) {
+      return callback && callback(err);
+    }
+  });
+}
+
+/* 
+Helper function to write `newData` to json file located at `filePath` 
+*/
+function jsonWriter(filePath, newData) {
+  stringifyData = JSON.stringify(newData, null, 2);
+  fs.writeFile(filePath, stringifyData, err => {
+    if (err) {
+      throw err;
+    }
+  });
+}
+
 
 module.exports = stockRoutes;
